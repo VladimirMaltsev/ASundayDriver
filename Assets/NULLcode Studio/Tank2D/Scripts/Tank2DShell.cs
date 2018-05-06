82a7d5a5fa3d300b428e749ac87f9ace5c210444
VladimirMaltsev <vov.maltsev@gmail.com>
2018-05-06 20:08:30 +0300

    include assets

---
+++ Assets/NULLcode Studio/Tank2D/Scripts/Tank2DShell.cs
@@ -0,0 +1,56 @@
+﻿/**************************************************************************/
+/** 	© 2016 NULLcode Studio. License: CC 0.
+/** 	Разработано специально для http://null-code.ru/
+/** 	WebMoney: R209469863836. Z126797238132, E274925448496, U157628274347
+/** 	Яндекс.Деньги: 410011769316504
+/**************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class Tank2DShell : MonoBehaviour {
+
+	[SerializeField] private float speed; // скорость снаряда
+	[SerializeField] private float damage; // наносимый урон
+	[SerializeField] private string[] tagList; // фильтр по тегам
+	[SerializeField] private LayerMask layers; // или по слоям
+
+	public void SetDirection(Vector3 direction)
+	{
+		Rigidbody2D body = GetComponent<Rigidbody2D>();
+		body.gravityScale = 0;
+		body.velocity = direction.normalized * speed;
+	}
+
+	bool Check(GameObject obj)
+	{
+		if(((1 << obj.layer) & layers) != 0)
+		{
+			return true;
+		}
+
+		foreach(string t in tagList)
+		{
+			if(obj.tag == t) return true;
+		}
+
+		return false;
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if(!coll.isTrigger && Check(coll.gameObject))
+		{
+			HPObject HP = coll.GetComponent<HPObject>();
+
+			if(HP != null)
+			{
+				HP.Adjust(-damage);
+			}
+		}
+
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/GameManage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManage : MonoBehaviour {
+
+    public bool gameIsOver;
+	// Use this for initialization
+	void Start () {
+        gameIsOver = false; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnGUI()
+    {
+        if (gameIsOver)
+        {
+            GUIStyle style = GUI.skin.textField;
+            style.fontSize = 25;
+            style.alignment = TextAnchor.MiddleCenter;
+            float hCenter = Screen.height / 2.0f;
+            GUI.Label(new Rect(0, hCenter, Screen.width, Screen.height / 5.0f), "Game over!", style);
+            style.fontSize = 20;
+            if (GUI.Button(new Rect(0, hCenter + Screen.height / 5f + 5, Screen.width / 2.0f, Screen.height / 10f), "Restart", style))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+            if (GUI.Button(new Rect(Screen.width / 2.0f, hCenter + Screen.height / 5f + 5, Screen.width / 2.0f, Screen.height / 10f), "Exit", style))
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
