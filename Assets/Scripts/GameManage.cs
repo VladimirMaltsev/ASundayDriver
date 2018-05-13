bf1ece3b48eeccedd8ac159bf6a1dda4903313ac
VladimirMaltsev <vov.maltsev@gmail.com>
2018-05-13 13:55:32 +0300

    create window: whach add and play

--- Assets/Scripts/GameManage.cs
+++ Assets/Scripts/GameManage.cs
@@ -1,44 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManage : MonoBehaviour {
 
-    public bool gameIsOver;
-	// Use this for initialization
-	void Start () {
-        gameIsOver = false; 
-	}
+    private float score;
+    public GameObject saveMenuUI;
+    public CarControllerGyro ccg;
+
+    public static string levelNamep;
+
+	void Awake () {
+        saveMenuUI.SetActive(false);
+        score = 0;
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+        if (ccg.car_speed > 0)
+            UpdateScore();
 	}
+
+    public void UpdateScore()
+    {
+            score += 0.1f;
+    }
+
+
+    public void ReloadLevel()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene(1);
+    }
 
     void OnGUI()
     {
-        if (gameIsOver)
-        {
-            GUIStyle style = GUI.skin.textField;
-            style.fontSize = 25;
-            style.alignment = TextAnchor.MiddleCenter;
-            float hCenter = Screen.height / 2.0f;
-            GUI.Label(new Rect(0, hCenter, Screen.width, Screen.height / 5.0f), "Game over!", style);
-            style.fontSize = 20;
-            if (GUI.Button(new Rect(0, hCenter + Screen.height / 5f + 5, Screen.width / 2.0f, Screen.height / 10f), "Restart", style))
-            {
-                Application.LoadLevel(Application.loadedLevel);
-            }
-            if (GUI.Button(new Rect(Screen.width / 2.0f, hCenter + Screen.height / 5f + 5, Screen.width / 2.0f, Screen.height / 10f), "Exit", style))
-            {
-                Application.Quit();
-            }
-        }
+        GUIStyle style = GUI.skin.label;
+        style.fontSize = 30;
+        style.alignment = TextAnchor.MiddleLeft;
 
-        if (GUI.Button(new Rect(0, Screen.height - 100, 200, 50), "Restart"))
-        {
-            Application.LoadLevel(index: Application.loadedLevel);
-            
-        }
+        GUI.Label(new Rect(10, 30, 500, 50), "RotationRateX = " + ccg.gyro.rotationRate.x, style);
+        GUI.Label(new Rect(10, 60, 500, 50), "RotationRateY = " + ccg.gyro.rotationRate.y, style);
+        GUI.Label(new Rect(10, 90, 500, 50), "RotationRateZ = " + ccg.gyro.rotationRate.z, style);
+        GUI.Label(new Rect(10, 120, 500, 50), "Angle = " + ccg.current_angle_rotation, style);
+        GUI.Label(new Rect(10, 150, 500, 50), "AngleAttitudeZ = " + ccg.current_gyro_rotation_z, style);
+        style.fontSize = 80;
+        style.normal.textColor = Color.white;
+        style.alignment = TextAnchor.MiddleCenter;
+        GUI.Label(new Rect(Screen.width / 2.0f - 125, 50, 250, 100), "" + (Mathf.Ceil(score) - 1), style);
     }
 }
